7757f26ed544cc14572a348f349b55140f172d7b
Gitea <gitea@fake.local>
2020-08-24 09:40:42 +0800

    first commit

---
+++ run.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yw
+ * Date: 2020/8/24
+ * Time: 9:35
+ */
+
+require  'vendor\autoload.php';
+
+use Src\qiefen;
+
+$test = new qiefen();
+$test->test();
---
+++ src/qiefen.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yw
+ * Date: 2020/8/24
+ * Time: 9:34
+ */
+
+namespace Src;
+
+
+class qiefen
+{
+    public function test()
+    {
+        var_dump(123);
+    }
+}
